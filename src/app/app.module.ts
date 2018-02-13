234e827a861e52b3bee6cd8054c98acdd8039fd4
unknown <dineeshsz@gmail.com>
2018-02-13 15:09:59 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MatToolbarModule, MatIconModule, 
+  MatFormFieldModule, MatInputModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    MyApp,
+    MyApp,  
     HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    BrowserAnimationsModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, NgZone } from '@angular/core';
 
+import { Platform } from 'ionic-angular';
+
+declare var ApiAIPromises: any;
+export class Message {
+  constructor(public content: string, public sentBy: string) {}
+}
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  messages = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public platform: Platform, public ngZone: NgZone) {
+    platform.ready().then(() => {
 
+      ApiAIPromises.init({
+        clientAccessToken: "09af47400a3441c6ad33b01ca04b3531"
+      })
+      .then((result) =>  console.log(result))
+        
+    });
   }
 
+  ask(question) {
+    this.messages.push(new Message(question, "user"));
+    ApiAIPromises.requestText({
+      query: question
+    })
+    .then(({result: {fulfillment: {speech}}}) => {
+       this.ngZone.run(()=> {
+         this.messages.push(new Message(speech, "bot"));
+       });
+    })
+  }
 }
